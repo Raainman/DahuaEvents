5be0ae3eb52037b6e232fd2173a0b38d13a31566
Raainman <raymond.koppen@gmail.com>
2020-01-29 15:18:40 +0100

    Update CameraEvents.py

--- CameraEvents.py
+++ CameraEvents.py
@@ -257,12 +257,11 @@ class DahuaDevice():
                 Alarm["channel"] = self.Name + ":" + str(index)
 
             if Alarm["Code"] == "VideoMotion":
-                VideoMotionData = json.loads(Alarm["data"])
+				VideoMotionData = json.loads(Alarm["data"])
 				#RegionName = json.dumps ( VideoMotionData )
-				
-                _LOGGER.info("Video Motion received: "+  Alarm["name"] + " Index: " + Alarm["channel"] + " Code: " + Alarm["Code"] + " RegionName " )
+				_LOGGER.info("Video Motion received: "+  Alarm["name"] + " Index: " + Alarm["channel"] + " Code: " + Alarm["Code"] + " RegionName " )
 				_LOGGER.info("Region: " + RegionName)
-                if Alarm["action"] == "Start":
+				if Alarm["action"] == "Start":
                     if not self.client.connected_flag:
                         self.client.reconnect()
                     self.client.publish(self.basetopic +"/" + Alarm["Code"] + "/" + Alarm["channel"] ,"ON")
